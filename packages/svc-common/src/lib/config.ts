60e0db511a90ffc9a6de3844c68e95b25e3ccd7b
Ivan Vityaev <godofluck89@gmail.com>
2023-03-21 16:32:51 +0300

    fix

--- packages/svc-common/src/lib/config.ts
+++ packages/svc-common/src/lib/config.ts
@@ -1,17 +1,24 @@
 
 export const config = {
-	secret: process.env.SECRET || "secret",
+	// for all services
 	mqttUrl: process.env.MQTT || "mqtt://localhost:1883",
-	redisUrl: process.env.REDIS || "redis://localhost:6379",
-	admin: {
-		login: "admin",
-		password: process.env.ADMIN_PASSWORD || "123"
-	},
+
+	// for all services with DB usage
 	pg: {
 		host: process.env.PG_HOST ||'localhost',
 		port: Number(process.env.PG_PORT || 5432),
 		username: process.env.PG_USER || 'postgres',
 		password: process.env.PG_PASSWORD || 'postgres',
 		database: process.env.PG_DB || 'postgres',
+	},
+
+	// for CALL svc
+	redisUrl: process.env.REDIS || "redis://localhost:6379",
+
+	// for USERS svc
+	secret: process.env.SECRET || "secret",
+	admin: {
+		login: "admin",
+		password: process.env.ADMIN_PASSWORD || "123"
 	}
 }
