01292c7dce46627d089309db39dea366141827b6
Ivan Vityaev <godofluck89@gmail.com>
2023-03-28 15:11:41 +0300

    fix

---
+++ packages/ui-admin/src/main.tsx
@@ -0,0 +1,24 @@
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
+import { AuthProvider, GlobalStyle, Protected, UserApi } from '@betacall/ui-kit';
+import { Home } from './components/Home';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Layout>
+    <GlobalStyle />
+    <AuthProvider>
+      <Protected role={UserApi.Role.ADMIN}>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </BrowserRouter>
+      </Protected>
+    </AuthProvider>
+  </Layout>
+);
