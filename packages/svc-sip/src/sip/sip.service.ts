f28c22d470c6ffa22e26d3b90caff1b4287fda74
Ivan Vityaev <godofluck89@gmail.com>
2023-04-19 16:04:23 +0300

    fix

--- packages/svc-sip/src/sip/sip.service.ts
+++ packages/svc-sip/src/sip/sip.service.ts
@@ -2,8 +2,7 @@ import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { Connection, Event } from "./node-esl-src"
 import { config } from "@betacall/svc-common";
 import { EventEmitter } from "stream";
-
-type IEventCallback = (e: Event) => void;
+import { v4 as uid } from 'uuid';
 
 @Injectable()
 export class SipService implements OnModuleInit {
@@ -11,36 +10,69 @@ export class SipService implements OnModuleInit {
 	private fs: Connection;
 
 	private handleReady = () => {
-		this.log("Ready");
-		this.callSelf('1000')
+		this.fs.subscribe([
+			'CHANNEL_DESTROY',
+			'CHANNEL_CREATE',
+			"CHANNEL_STATE",
+			"BACKGROUND_JOB"
+		], () => {
+			this.log("Ready");
+			this.callUser('1000');
+		})
+	}
+
+	private getMessage = (e: Event) => {
+		const [ status, ...data ] = e.getBody().split(' ');
+		return {
+			status,
+			isError: status === "-ERR" || status === "-USAGE:",
+			data: data.join(' ')
+		}
 	}
 
 	private events = new EventEmitter();
 
-	private handleEvt = (evt: Event) => {
+	private handleAllEvents = (evt: Event) => {
 		const evtName = evt.getType();
-		console.log(evtName)
-		const [ status, message ] = evt.getBody().split(" ") as string[];
-		if (status === "-ERR")
-			throw new Error(message);
-
-		return message;
+		if (evtName === "BACKGROUND_JOB") {
+			const uuid = evt.getHeader('Job-UUID');
+			this.events.emit(uuid, evt);
+		}
 	}
 
-	private handleCall: IEventCallback = (e) => {
-		const uid = this.handleEvt(e);
-		this.fs.execute('playback', '/usr/share/freeswitch/sounds/transfer1.wav', uid);
+	private async originate (url: string, phone: string = '') {
+		const id = uid();
+		this.fs.bgapi('originate', `${url} ${phone} &park()`, id);
+		const evt = await this.result(id);
+		const msg = this.getMessage(evt);
+		if (msg.isError)
+			throw new Error(msg.data);
+		return msg.data;
 	}
 
-	private callSelf = (phone: string) => {
-		const uid = this.fs.api('originate', [`sofia/internal/ext@192.168.89.249:61924 ${phone}`]);
-		this.fs.execute('playback', '/usr/share/freeswitch/sounds/transfer1.wav');
-		return;
-		this.fs.bgapi('originate', [`sofia/internal/ext@192.168.89.149:56548 ${phone}`]);
+	private async playRecord (uuid: string) {
+		const id = uid();
+		const app = 'uuid_broadcast'
+		const cmd = `${uuid} /usr/share/freeswitch/sounds/transfer1.wav both`
+		this.fs.bgapi(app, cmd, id);
+		const evt = await this.result(id);
+		return this.getMessage(evt);
 	}
 
-	private callExternal = (phone: string) => {
-		this.fs.bgapi('originate', [`sofia/gateway/freesw1/${phone} &park()`]);
+	private result (uid: string): Promise<Event> {
+		return new Promise((res) => {
+			this.events.once(uid, res);
+		});
+	}
+
+	private callUser = async (phone: string) => {
+		const callid = await this.originate(`user/${phone}`);
+		await this.playRecord(callid);
+	}
+
+	private callExternal = async (phone: string) => {
+		const callid = await this.originate('sofia/gateway/freesw1/ext', phone);
+		await this.playRecord(callid);
 	}
 
 	private log = (msg: string) => {
@@ -49,15 +81,7 @@ export class SipService implements OnModuleInit {
 
 	private connect () {
 		this.fs = Connection.createInbound(config.sip, config.sip.password, this.handleReady)
-		this.fs.subscribe([
-			'CHANNEL_CREATE',
-			'CHANNEL_CALLSTATE',
-			// 'CHANNEL_STATE',
-			// 'CHANNEL_EXECUTE',
-			// 'CHANNEL_EXECUTE_COMPLETE',
-			'CHANNEL_DESTROY'
-		]);
-		this.fs.on('esl::event::**', this.handleEvt)
+		this.fs.on('esl::event::**', this.handleAllEvents)
 	}
 
 	onModuleInit() {
