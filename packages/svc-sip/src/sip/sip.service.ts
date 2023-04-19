baa1446660a7a5f7e24222225534a69d5b78fa4d
Ivan Vityaev <godofluck89@gmail.com>
2023-04-19 09:00:55 +0300

    fix

--- packages/svc-sip/src/sip/sip.service.ts
+++ packages/svc-sip/src/sip/sip.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
-import { Connection } from "./node-esl"
+import { Connection, Event } from "./node-esl"
 import { IEventCallback } from "./node-esl/src/esl/Connection";
 import { config } from "@betacall/svc-common";
+import { EventEmitter } from "stream";
 @Injectable()
 export class SipService implements OnModuleInit {
 
@@ -9,21 +10,32 @@ export class SipService implements OnModuleInit {
 
 	private handleReady = () => {
 		this.log("Ready");
-		this.fs.on('esl*', this.handleEvt);
+		this.fs.on('esl::event', this.handleEvt)
 		this.callSelf('1000')
 	}
 
-	private handleEvt: IEventCallback = (evt) => {
-		console.log(evt['_body'])
+	private events = new EventEmitter();
+
+	private handleEvt = (evt: Event) => {
+		const evtName = evt.getType();
+		console.log(evtName)
+		const [ status, message ] = evt.getBody().split(" ") as string[];
+		if (status === "-ERR")
+			throw new Error(message);
+
+		return message;
 	}
 
-	private status = () => {
-		this.fs.executeAsync('sofia', 'status', this.handleEvt)
+	handleCall: IEventCallback = (e) => {
+		const uid = this.handleEvt(e);
+		this.fs.execute('playback', '/usr/share/freeswitch/sounds/transfer1.wav', uid, this.handleEvt);
 	}
-
 
 	callSelf = (phone: string) => {
-		this.fs.bgapi(`originate sofia/internal/ext@192.168.89.149:56548`, [phone], this.handleEvt);
+		this.fs.api('originate', `sofia/internal/ext@192.168.89.149:56548 ${phone}`, this.handleCall);
+		this.fs.execute('playback', '/usr/share/freeswitch/sounds/transfer1.wav', this.handleEvt);
+		return;
+		this.fs.bgapi('originate', `sofia/internal/ext@192.168.89.149:56548 ${phone}`, this.handleCall);
 	}
 
 	callExternal = (phone: string) => {
@@ -39,5 +51,14 @@ export class SipService implements OnModuleInit {
 			host: config.sip.host,
 			port: config.sip.port
 		}, config.sip.password, this.handleReady)
+		this.fs.subscribe([
+			'CHANNEL_CREATE',
+			'CHANNEL_CALLSTATE',
+			// 'CHANNEL_STATE',
+			// 'CHANNEL_EXECUTE',
+			// 'CHANNEL_EXECUTE_COMPLETE',
+			'CHANNEL_DESTROY'
+		]);
+		this.fs.on('esl::event::**', this.handleEvt)
 	}
 }
